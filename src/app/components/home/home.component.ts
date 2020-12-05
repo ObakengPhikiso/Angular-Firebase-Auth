4a9d4fb35b52659b09027b30af0386d74cfb8411
Obakeng <obakeng.phikiso@gmail.com>
2020-12-05 11:31:06 +0200

    final

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,7 +4,7 @@ import { AuthService } from 'src/app/services/auth.service';
 import { HomeService } from '../../services/home.service';
 import { AuthLogin } from 'src/app/interfaces/auth';
 import { form } from 'src/app/interfaces/auth';
-import {  AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
 
 
 
@@ -14,46 +14,45 @@ import {  AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  
+
 
 
   constructor(private readonly authService: HomeService, private fb: FormBuilder, private firestore: AngularFirestore) {
-   
-    
-   }
-  
+
+
+  }
+
   ourForm = new FormGroup({
 
-    department:new FormControl('',[Validators.required]),
-    school: new FormControl('',[Validators.required]),
-    moduleName: new FormControl('',[Validators.required]),
-     moduleCode: new FormControl('',[Validators.required]),
-     nameOfSender: new FormControl( '',[Validators.required]),
-     moduleLecture: new FormControl( '',[Validators.required]),
-    moduleYear: new FormControl('',[Validators.required]),
-    studentEnrolled:  new FormControl('',[Validators.required]),
-    reasonRequest:  new FormControl('',[Validators.required]),
-    tutorName:new FormControl( '',[Validators.required]),
-    tutorSurname: new FormControl( '',[Validators.required]),
-    studentNum:  new FormControl('',[Validators.required]),
-    yearStudy: new FormControl('',[Validators.required]),
-     sessionNum: new FormControl('',[Validators.required]),
-    numHours: new FormControl('',[Validators.required])
-    
+    department: new FormControl('', [Validators.required]),
+    school: new FormControl('', [Validators.required]),
+    moduleName: new FormControl('', [Validators.required]),
+    moduleCode: new FormControl('', [Validators.required]),
+    nameOfSender: new FormControl('', [Validators.required]),
+    moduleLecture: new FormControl('', [Validators.required]),
+    moduleYear: new FormControl('', [Validators.required]),
+    studentEnrolled: new FormControl('', [Validators.required]),
+    reasonRequest: new FormControl('', [Validators.required]),
+    tutorName: new FormControl('', [Validators.required]),
+    tutorSurname: new FormControl('', [Validators.required]),
+    studentNum: new FormControl('', [Validators.required]),
+    yearStudy: new FormControl('', [Validators.required]),
+    sessionNum: new FormControl('', [Validators.required]),
+    numHours: new FormControl('', [Validators.required])
+
   })
-    
+
 
   ngOnInit(): void {
-    
-  }
-  
-  submitData(data: form): any {
-    this.authService.submitData(data);
+
   }
 
-    onSubmit()
-    {
-      alert(JSON.stringify(this.ourForm.value))
-    }
+  submitData(data: form): any {
+    this.authService.submitData(data).then(() => {
+      console.log('created');
+    }).catch(err => {
+      console.log(err.message);
+    });
+  }
 
 }
--- src/app/interfaces/auth.ts
+++ src/app/interfaces/auth.ts
@@ -10,24 +10,23 @@ export interface AuthLogin {
     email: string;
     password: string;
 }
-export interface form
-{
-nameOfSender: string;
-department: string;
-school: string;
- moduleName : string;
- moduleCode : string;
- moduleLecture : string;
- moduleYear : number;
- studentEnrolled : number;
- reasonRequest : string;
- tutorName : string;
- tutorSurname : string;
- studentNum : number;
- sessionNum : number;
- numHours : number; 
- yearStudy: number
- 
+export interface form {
+    nameOfSender: string;
+    department: string;
+    school: string;
+    moduleName: string;
+    moduleCode: string;
+    moduleLecture: string;
+    moduleYear: number;
+    studentEnrolled: number;
+    reasonRequest: string;
+    tutorName: string;
+    tutorSurname: string;
+    studentNum: number;
+    sessionNum: number;
+    numHours: number;
+    yearStudy: number
 
-       
+
+
 }
