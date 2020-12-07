9c5c8073b61f687efad044309bda46b93e9031e8
Obakeng Phikiso <obakeng@thedigitalacademy.co.za>
2020-12-07 10:42:32 +0200

    server.js

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const app = express();
-app.use(express.static('./dist/firebase-auth"'));
+app.use(express.static('./dist/firebase-auth'));
 app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/firebase-auth"/'}
+  res.sendFile('index.html', {root: 'dist/firebase-auth/'}
 );
 });
-
 app.listen(process.env.PORT || 8080);
