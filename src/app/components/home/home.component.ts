de4554705ec4ce071b62e9434fb5ac52448b00f4
obakeng1 <obakeng@thedigitalacademy.co.za>
2021-03-02 01:25:58 +0200

    iterate a field

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, Validators, FormControl, FormsModule, FormBuilder } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormControl, FormsModule, FormBuilder, FormArray } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { HomeService } from '../../services/home.service';
 import { AuthLogin } from 'src/app/interfaces/auth';
@@ -14,67 +14,34 @@ import { Router } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  myFormValueChanges$;
+  ourForm: FormGroup;
+  constructor(private readonly authService: HomeService,
+    private firestore: AngularFirestore, private router: Router, private formBuilder: FormBuilder,
+  ) { }
 
-
-
-  constructor(private readonly authService: HomeService, private fb: FormBuilder,
-    private firestore: AngularFirestore, private router: Router) {
-
+  ngOnInit(): void {
+    this.ourForm = this.formBuilder.group({
+      department: new FormControl('', [Validators.required]),
+      school: new FormControl('', [Validators.required]),
+      moduleName: new FormControl('', [Validators.required]),
+      moduleCode: new FormControl('', [Validators.required]),
+      nameOfSender: new FormControl('', [Validators.required]),
+      moduleLecture: new FormControl('', [Validators.required]),
+      moduleYear: new FormControl('', [Validators.required]),
+      studentEnrolled: new FormControl('', [Validators.required]),
+      reasonRequest: new FormControl('', [Validators.required]),
+      tutors: this.formBuilder.array([
+        this.getTutor()
+      ]),
+    })
+    this.myFormValueChanges$ = this.ourForm.controls['tutors'].valueChanges;
 
   }
 
-  ourForm = new FormGroup({
-
-    department: new FormControl('', [Validators.required]),
-    school: new FormControl('', [Validators.required]),
-    moduleName: new FormControl('', [Validators.required]),
-    moduleCode: new FormControl('', [Validators.required]),
-    nameOfSender: new FormControl('', [Validators.required]),
-    moduleLecture: new FormControl('', [Validators.required]),
-    moduleYear: new FormControl('', [Validators.required]),
-    studentEnrolled: new FormControl('', [Validators.required]),
-    reasonRequest: new FormControl('', [Validators.required]),
-    tutorName: new FormControl('', [Validators.required]),
-    tutorSurname: new FormControl('', [Validators.required]),
-    studentNum: new FormControl('', [Validators.required]),
-    tutorName_2: new FormControl('', [Validators.required]),
-    tutorSurname_2: new FormControl('', [Validators.required]),
-    studentNum_2: new FormControl('', [Validators.required]),
-    tutorName_3: new FormControl('', [Validators.required]),
-    tutorSurname_3: new FormControl('', [Validators.required]),
-    studentNum_3: new FormControl('', [Validators.required]),
-    tutorName_4: new FormControl('', [Validators.required]),
-    tutorSurname_4: new FormControl('', [Validators.required]),
-    studentNum_4: new FormControl('', [Validators.required]),
-    tutorName_5: new FormControl('', [Validators.required]),
-    tutorSurname_5: new FormControl('', [Validators.required]),
-    studentNum_5: new FormControl('', [Validators.required]),
-    tutorName_6: new FormControl('', [Validators.required]),
-    tutorSurname_6: new FormControl('', [Validators.required]),
-    studentNum_6: new FormControl('', [Validators.required]),
-    tutorName_7: new FormControl('', [Validators.required]),
-    tutorSurname_7: new FormControl('', [Validators.required]),
-    studentNum_7: new FormControl('', [Validators.required]),
-    tutorName_8: new FormControl('', [Validators.required]),
-    tutorSurname_8: new FormControl('', [Validators.required]),
-    studentNum_8: new FormControl('', [Validators.required]),
-    tutorName_9: new FormControl('', [Validators.required]),
-    tutorSurname_9: new FormControl('', [Validators.required]),
-    studentNum_9: new FormControl('', [Validators.required]),
-    tutorName_10: new FormControl('', [Validators.required]),
-    tutorSurname_10: new FormControl('', [Validators.required]),
-    studentNum_10: new FormControl('', [Validators.required]),
-
-    yearStudy: new FormControl('', [Validators.required]),
-    sessionNum: new FormControl('', [Validators.required]),
-    numHours: new FormControl('', [Validators.required])
-
-  })
-
-
-  ngOnInit(): void {
-
+  ngOnDestroy(): void {
+    this.myFormValueChanges$.unsubscribe();
   }
 
   submitData(data: form): any {
@@ -85,4 +52,33 @@ export class HomeComponent implements OnInit {
     });
   }
 
+  getTutor() {
+    return this.formBuilder.group({
+      tutorName: ['', Validators.required],
+      tutorSurname: ['', Validators.required],
+      studentNum: ['', Validators.required],
+      yearStudy: ['', Validators.required],
+      sessionNum: ['', Validators.required],
+      numHours: ['', Validators.required]
+    })
+  }
+
+  addTutor() {
+    const control = <FormArray>this.ourForm.controls['tutors'];
+    control.push(this.getTutor());
+  }
+
+  removeTutor(i: number) {
+    const control = <FormArray>this.ourForm.controls['tutors'];
+    control.removeAt(i);
+  }
+
+  clearAllTutors() {
+    const control = <FormArray>this.ourForm.controls['tutors'];
+    while (control.length) {
+      control.removeAt(control.length - 1);
+    }
+    control.clearValidators();
+    control.push(this.getTutor());
+  }
 }
